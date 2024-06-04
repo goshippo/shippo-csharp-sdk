6ff6c7fd0ecfd0c56ee279eba84b8be0907f2431
ma575081 <91095107+ma575081@users.noreply.github.com>
2024-06-04 07:31:50 -0500

    feat: add ShippoAuthBeforeRequestHook

--- Shippo/Hooks/HookRegistration.cs
+++ Shippo/Hooks/HookRegistration.cs
@@ -1,36 +1,42 @@
+namespace Shippo.Hooks;
 
-namespace Shippo.Hooks
+using AuthenticationHeaderValue = System.Net.Http.Headers.AuthenticationHeaderValue;
+using HttpRequestMessage = System.Net.Http.HttpRequestMessage;
+using Task = System.Threading.Tasks.Task;
+using TaskHttpRequestMessage =
+    System.Threading.Tasks.Task<System.Net.Http.HttpRequestMessage>;
+
+/// <summary>
+/// Hook Registration File.
+/// </summary>
+
+public class ShippoAuthBeforeRequestHook : IBeforeRequestHook
+{
+    public const string SCHEME = "ShippoToken";
+
+    public async TaskHttpRequestMessage BeforeRequestAsync(
+        BeforeRequestContext hookCtx, HttpRequestMessage request)
+    {
+        return await Task.Run(() =>
+        {
+            string scheme = request.Headers.Authorization?.Scheme ?? "";
+            if (scheme.StartsWith("shippo_"))
+            {
+                request.Headers.Authorization =
+                    new AuthenticationHeaderValue(SCHEME, scheme);
+            }
+            return request;
+        });
+    }
+}
+
+public static class HookRegistration
 {
     /// <summary>
-    /// Hook Registration File.
+    /// Initializes hooks.
     /// </summary>
-    /// <remarks>
-    /// This file is only ever generated once on the first generation and then is free to be modified.
-    /// Any hooks you wish to add should be registered in the InitHooks function. Feel free to define them
-    /// in this file or in separate files in the Hooks folder.
-    /// </remarks>
-    public static class HookRegistration
+    public static void InitHooks(IHooks hooks)
     {
-        /// <summary>
-        /// Initializes hooks.
-        /// </summary>
-        /// <remarks>
-        /// Add hooks by calling `Shippo.Hooks.Register&lt;HookInterface&gt;(myHook);`
-        /// where `I&lt;HookInterface&gt;` is one of the following interfaces defined in HookTypes.cs:
-        ///   - ISDKInitHook
-        ///   - IBeforeRequestHook
-        ///   - IAfterSuccess
-        ///   - IAfterError
-        /// and `myHook` an instance that implements that specific interface.
-        /// </remarks>
-        public static void InitHooks(IHooks hooks)
-        {
-            // var myHook = new MyHook();
-
-            // hooks.RegisterSDKInitHook(myHook);
-            // hooks.RegisterBeforeRequestHook(myHook);
-            // hooks.RegisterAfterSuccessHook(myHook);
-            // hooks.RegisterAfterErrorHook(myHook;
-        }
+        hooks.RegisterBeforeRequestHook(new ShippoAuthBeforeRequestHook());
     }
 }
